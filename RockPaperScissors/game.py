e4ff138f38a7fc45f192e5d2b098a482b7cb5d85
Dragneel343 <37350944+Dragneel343@users.noreply.github.com>
2020-10-02 12:57:57 -0600

    final changes

--- RockPaperScissors/game.py
+++ RockPaperScissors/game.py
@@ -11,6 +11,7 @@ player_name = input("Hello, What's your name?\n")
 
 #set player to False
 player = False
+#set play_again to Yes
 play_again = "Yes"
 
 while player == False and play_again == "Yes":
@@ -18,23 +19,31 @@ while player == False and play_again == "Yes":
     player = input("Rock, Paper, Scissors?\n")
     if player == computer:
         print("It's a Tie!")
+
     elif player == "Rock":
         if computer == "Paper":
             print(player_name, "You lose!\n", computer, "covers", player)
+
         else:
             print(player_name, "You win!\n", player, "smashes", computer)
+
     elif player == "Paper":
         if computer == "Scissors":
             print(player_name, "You lose!\n", computer, "cut", player)
+
         else:
             print(player_name, "You win!\n", player, "covers", computer)
+
     elif player == "Scissors":
         if computer == "Rock":
             print(player_name, "You lose!\n", computer, "smashes", player)
+
         else:
             print(player_name, "You win!\n", player, "cut", computer)
+
     else:
         print("That's not a valid play", player_name,". Check your spelling!")
+
     #the player is asked to play again if Yes, the play_again is set to Yes and player is set to False.
     #if No, then play_again is set to No and the game ends.
     play_again = input("Play Again? Yes/No\n")
