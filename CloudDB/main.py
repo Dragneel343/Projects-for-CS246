8e645ecdaf4ebabb8b1c3b5bc6df0fb4152561f3
Dragneel343 <37350944+Dragneel343@users.noreply.github.com>
2020-10-27 16:03:50 -0600

    updated .gitignores

--- CloudDB/main.py
+++ CloudDB/main.py
@@ -1,11 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-import os
-import random
 
 # Use the application default credentials
-cred = credentials.Certificate('./Service_account_key.json')
+cred = credentials.Certificate('CloudDB/Service_account_key.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-return db
+#print(db)
+# results = db.collection("users").get()
+
+# for result in results:
+#     info = result.to_dict()
+#     print(info['Name'])
+
